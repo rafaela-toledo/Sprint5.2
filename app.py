7c5e31d0ffcb446322da446d3ffd4c6beb4e08bb
Rafaela Toledo <rafaelagotoledo@gmail.com>
2024-04-03 19:30:14 -0300

    Correção colunas gráfico de dispersão

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import pandas as pd
 import plotly.express as px
 
 data_car = pd.read_csv("vehicles.csv")
+
+# Botão para criar um histograma
 hist_button = st.button('Criar histograma') 
-     
 if hist_button: 
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.histogram(data_car, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    fig_hist = px.histogram(data_car, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
 
+# Botão para criar um gráfico de dispersão
 scatter_button = st.button('Criar gráfico de dispersão') 
-
 if scatter_button: 
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-    fig_scatter = px.scatter(data_car, x="x_coluna", y="y_coluna")
+    fig_scatter = px.scatter(data_car, x="odometer", y="price", title="Relação entre Quilometragem e Preço",
+                             labels={"odometer": "Quilometragem", "price": "Preço"})
     st.plotly_chart(fig_scatter, use_container_width=True)
